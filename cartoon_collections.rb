40cd31f11181ed71159e748436f98e5811901098
Toni L <tlam212@gmail.com>
2019-11-10 22:45:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,9 +4,9 @@ def roll_call_dwarves(array)
     end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def summon_captain_planet(array)
+  
+ end
 
 def long_planeteer_calls# code an argument here
   # Your code here
